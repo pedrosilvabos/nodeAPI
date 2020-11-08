c07ed5e61a6cd0df3a131d36e4055a9474fe4cef
JOAO PEDRO SILVA FARIA <j-pedro-faria@telecom.pt>
2020-11-07 23:24:17 -0100

    testing middleware on heroku

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
-
+//middleware
+app.use('/route1', ()=>{
+    console.log("we chose route 1")
+})
 //creating routs
 app.get('/', (req, res) =>{
     res.send('we are home');
